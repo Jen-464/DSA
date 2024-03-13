7dbbc3026814fd76b38953e4e48004c9d320fef0
Jen-464 <jenlopez464@gmail.com>
2024-03-12 18:05:52 -0700

    completed first bad version leetcode problem

---
+++ Week 2/1_first_bad_version/firstBadVersion.js
@@ -0,0 +1,44 @@
+/**
+ * Definition for isBadVersion()
+ * 
+ * @param {integer} version number
+ * @return {boolean} whether the version is bad
+ * isBadVersion = function(version) {
+ *     ...
+ * };
+ */
+
+/**
+ * @param {function} isBadVersion()
+ * @return {function}
+ */
+var solution = function(isBadVersion) {
+    /**
+     * @param {integer} n Total versions
+     * @return {integer} The first bad version
+     */
+    return function(n) {
+        let left = 1; 
+        let mid = Math.floor((n + 1) / 2); 
+        let right = n;
+        let foundFirstBadVersion = false;
+
+        while(foundFirstBadVersion == false) {
+            if(isBadVersion(mid) == false) {
+                left = left == mid ? mid + 1: mid;
+                mid = Math.floor((left + right) / 2);
+            }
+            else {
+                if(isBadVersion(mid-1)) {
+                    right = mid;
+                    mid = Math.floor((left + right) / 2);
+                }
+                else {
+                    return mid;
+                }
+            }
+        }
+    };
+};
+
+module.exports = solution;
---
+++ Week 2/1_first_bad_version/firstBadVersion.test.js
@@ -0,0 +1,15 @@
+"use strict";
+const solution = require("./firstBadVersion");
+
+describe("solution(nums, target)", () => {
+    // it("[-1,0,3,5,9,12] & 9 --> returns 4", () => {
+    //     let n = 5;
+    //     let bad = 4;
+    //     let result = solution(nums, target);
+    //     expect(result).toBe(4);
+    // })
+    it("test", () => {
+        let result = true;
+        expect(result).toBe(true);
+    })
+})
