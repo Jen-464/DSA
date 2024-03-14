cc536b5c2daff34d830afa52ae351944b24e5406
Jen-464 <jenlopez464@gmail.com>
2024-03-13 17:19:57 -0700

    tested my firstBadVersion function leetcode

--- Week 2/1_first_bad_version/firstBadVersion.test.js
+++ Week 2/1_first_bad_version/firstBadVersion.test.js
@@ -1,15 +1,34 @@
 "use strict";
 const solution = require("./firstBadVersion");
 
-describe("solution(nums, target)", () => {
-    // it("[-1,0,3,5,9,12] & 9 --> returns 4", () => {
-    //     let n = 5;
-    //     let bad = 4;
-    //     let result = solution(nums, target);
-    //     expect(result).toBe(4);
-    // })
-    it("test", () => {
-        let result = true;
-        expect(result).toBe(true);
-    })
-})
+var isBadVersion = function(val, bad) {
+    return val >= bad;
+}
+
+describe("solution(n, bad)", () => {
+    it("n = 5 & bad = 4 --> returns 4", () => {
+        let n = 5;
+        let bad = 4;
+        let result = solution(isBadVersion)(n, bad);
+        expect(result).toBe(4);
+    });
+    it("n = 1 & bad = 1 --> returns 1", () => {
+        let n = 1;
+        let bad = 1;
+        let result = solution(isBadVersion)(n, bad);
+        expect(result).toBe(1);
+    });
+    it("n = 8 & bad = 8 --> returns 1", () => {
+        let n = 8;
+        let bad = 8;
+        let result = solution(isBadVersion)(n, bad);
+        expect(result).toBe(8);
+    });
+    it("n = 50 & bad = 2 --> returns 2", () => {
+        let n = 50;
+        let bad = 2;
+        let result = solution(isBadVersion)(n, bad);
+        expect(result).toBe(2);
+    });
+    // Reducing of API calls to isBadVersion
+});
