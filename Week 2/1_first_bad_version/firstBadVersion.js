6c2fccb3a78f4451be51ed2d446766d9ba6ff8d4
Jen-464 <jenlopez464@gmail.com>
2024-03-13 17:19:27 -0700

    fixed function to work in VS Code

--- Week 2/1_first_bad_version/firstBadVersion.js
+++ Week 2/1_first_bad_version/firstBadVersion.js
@@ -17,19 +17,19 @@ var solution = function(isBadVersion) {
      * @param {integer} n Total versions
      * @return {integer} The first bad version
      */
-    return function(n) {
+    return function(n, bad) {
         let left = 1; 
         let mid = Math.floor((n + 1) / 2); 
         let right = n;
         let foundFirstBadVersion = false;
 
         while(foundFirstBadVersion == false) {
-            if(isBadVersion(mid) == false) {
+            if(isBadVersion(mid, bad) == false) { // if(isBadVersion(mid) == false) {
                 left = left == mid ? mid + 1: mid;
                 mid = Math.floor((left + right) / 2);
             }
             else {
-                if(isBadVersion(mid-1)) {
+                if(isBadVersion(mid-1, bad)) { // if(isBadVersion(mid-1)) {
                     right = mid;
                     mid = Math.floor((left + right) / 2);
                 }
